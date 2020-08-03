6865c76ad348d36c9f29ee7a021b4bb2d5e78773
NessMC <contact@nessmc.fr>
2020-08-03 16:45:11 +0200

    ✨ Parser class added.

---
+++ src/core/parser.ts
@@ -0,0 +1,26 @@
+import { scanner } from './scanner'
+
+export class Tokenizer {
+
+  public static tokens: Object = {}
+  public static customOut: Object = {}
+  public static ignore: Object = {}
+  public static functions: Object = {}
+
+  constructor() { }
+
+  public static addTokenSet (tokenSet: any) {
+
+    for (const key in tokenSet) {
+      this.tokens[key] = tokenSet[key]
+    }
+
+  }
+
+  public static tokenize (string: string) {
+
+    return scanner(string, this)
+
+  }
+
+}
