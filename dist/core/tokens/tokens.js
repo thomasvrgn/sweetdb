2373ccf2c453c2c9ae3ea8a353476d55ad706222
NessMC <contact@nessmc.fr>
2020-08-03 16:36:45 +0200

    🙈 Dist folder to git ignored files added.

--- dist/core/tokens/tokens.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Tokens list
-exports.default = {
-    DATABASE: /(db|database)/i,
-    TABLE: /table/i,
-    FIELD: /field/i,
-    PARAM: /-(\s+)?(\w|\s)+=>.*/i
-};
